897743b8895ce462b7c749d420b27328c60b2a46
Ferdi28 <49550626+Ferdi28@users.noreply.github.com>
2019-12-02 17:44:11 +0100

    added new data frames
    possible way of displaying on map

---
+++ R/Map_plot_function.R
@@ -0,0 +1,28 @@
+
+
+indeed_map <- function(switzerland,jobs_data_frame){
+
+  ggplot() +
+    labs( title = "Jobs location") +
+      geom_polygon(data = switzerland,aes(x = Longitude, y = Latitude),color = "black", alpha = 0.3) +
+      coord_fixed(1.3) +
+        geom_point(data = jobs_data_frame, aes(Longitude,Latitude),color= "steelblue") +
+
+    guides(size = guide_legend(order = 1),
+           color = guide_legend(order = 2)) +
+
+    theme(
+      legend.key = element_rect(fill=NA),
+      aspect.ratio = 0.75:0.75,
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "black"),
+      panel.border = element_rect(colour = "black", fill=NA, size=1))
+}
+
+indeed_map(switzerland, Map_ready_df)
+
+
+
+# a way to make an interractive map
+locations_sf <- Map_ready_df %>% na.omit() %>%  st_as_sf( coords = c("Longitude", "Latitude"), crs = 4326)
+mapview(locations_sf)
---
+++ R/scrape_geo.R
@@ -0,0 +1,28 @@
+ok <- data_economics_consulting_communications %>% group_by(city) %>% count() %>% na.omit() %>% as.data.frame()
+
+ok %>% filter(n == 1)
+
+ok1 <- ok %>% mutate_geocode(city)
+
+saveRDS(ok1, file = "geo_data.rds")
+
+register_google(key = "AIzaSyCGsH8T1ViNsk6pP0m5ustOJ17u4_dbCUU", write = TRUE)
+
+#transform in character in order to join them
+data_economics_consulting_communications <- data_economics_consulting_communications %>%
+  mutate(city = as.character(city))
+
+geo_data <- geo_data %>%
+  mutate(city = as.character(city))
+# join data
+data_eco <- left_join(data_economics_consulting_communications, geo_data, by = "city")
+
+data_eco <- data_eco[, - c(5,9)]
+names(data_eco)[8]<-paste("Longitude")
+names(data_eco)[9]<-paste("Latitude")
+
+saveRDS(data_eco, file = "new_data_economics.rds")
+
+# google map made some wrong calculations about some coordinates so we need to remove them
+Map_ready_df <- new_data_economics %>%
+  filter(between(Latitude,45.83203, 47.69732) & between(Longitude, 6.07544, 9.83723 ))
