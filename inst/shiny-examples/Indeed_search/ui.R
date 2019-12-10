ce5fb06d57c594b9dd80e04ea52ca92112e55c2d
Ferdi28 <49550626+Ferdi28@users.noreply.github.com>
2019-12-10 18:08:05 +0100

    Update shiny UI
    add more clarity to sidebar layout and text

--- inst/shiny-examples/Indeed_search/ui.R
+++ inst/shiny-examples/Indeed_search/ui.R
@@ -10,24 +10,31 @@ shinyUI(fluidPage(theme = "bootstrap.css",
     sidebarLayout(
 
         sidebarPanel(
-            tags$div(
-                HTML("Welcome to our Shiny App, here is a little guide to our app: </br>
-                          </br> - Load our latest dataset of Indeed Job listings. An initial map with all the job listings in Switzerland will be displayed.
-                               </br> - If you would like to filter by Job Category, make sure to select them and click the <strong>Confirm Choice </strong> button.
-                              </br> - Selecting the <strong>Map With Selected Categories</strong> option will display the map with the filters you chose.
-                              </br> - You can update the map with the filters by clicking on <strong>Confirm Choice</strong> and having the <strong>Map
+                        tags$div(
+                                HTML("<strong>Welcome to our Shiny App, here is a little guide to our app:</strong> </br></br>
+                                 - Load our latest dataset of Indeed Job listings. An initial map with all the job listings in Switzerland will be displayed.
+                               </br> </br>- If you would like to filter by Job Category, make sure to select them and click the <strong>Confirm Choice </strong> button.
+                              </br> </br> - Selecting the <strong>Map With Selected Categories</strong> option will display the map with the filters you chose.
+                              </br> </br> - You can update the map with the filters by clicking on <strong>Confirm Choice</strong> and having the <strong>Map
                                With Selected Categories</strong> selected!
-                               </br> - If you would like to go back to the initial map, simply select <strong>Map With All Job Types</strong> in <strong>Choose Map</strong>.")),
-            fileInput("file1", "Choose Indeed RDS File",
+                               </br> </br>- If you would like to go back to the initial map,
+                                simply select <strong>Map With All Job Types</strong> in <strong>Choose Map</strong>.</br></br>")),
+
+                         fileInput("file1", "Choose Indeed RDS File",
                                 multiple = FALSE,
                                 accept = c(".rds", ".RDS")),
-            tags$style(type="text/css",
-                       ".shiny-output-error { visibility: hidden; }",
-                       ".shiny-output-error:before { visibility: hidden; }"
-            ),
+
+                        tags$style(type="text/css",
+                                ".shiny-output-error { visibility: hidden; }",
+                                ".shiny-output-error:before { visibility: hidden; }" ),
+
+                        tags$style(
+                        HTML(".sidebar { height: 90vh; overflow-y: auto; }" )),
 
                       actionButton(inputId = "go",
                                    label = "Load Data Base"),
+                      tags$div(
+                          HTML("</br>")),
 
                       selectInput(inputId = "category",
                                   choices = c("Marketing", "HR", "Data Science", "Consulting",
@@ -38,6 +45,9 @@ shinyUI(fluidPage(theme = "bootstrap.css",
 
                       actionButton(inputId = "choose", label = "Confirm Choice"),
 
+                      tags$div(
+                          HTML("</br>")),
+
                       selectInput(inputId = "map",
                                   label = "Choose the Map",
                                   choices = c("Map With All Job Types", "Map With Selected Categories"))
@@ -46,7 +56,7 @@ shinyUI(fluidPage(theme = "bootstrap.css",
 
         mainPanel(
 
-            leafletOutput("Map"),
+            leafletOutput("Map")
         )
     )
 ))
