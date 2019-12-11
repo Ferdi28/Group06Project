cacb9d5c8ebbca66743c13cfa6f7f29c34fb0446
Ferdi28 <ferdinando.pepe30@gmail.com>
2019-12-11 17:09:01 +0100

    Update shiny app
    add indentation, more precise description and better base value for map size

--- inst/shiny-examples/Indeed_search/server.R
+++ inst/shiny-examples/Indeed_search/server.R
@@ -4,44 +4,47 @@ library(leaflet)
 library(tidyverse)
 
 shinyServer(function(input, output) {
-  #make sure leaflet output is correct size
+
+  #make Leaflet output change in size based on user preferences
   output$leaf=renderUI({
     leafletOutput('map', width = "100%", height = input$Height)
   })
- #load data as a reactive event
+
+  #load data as a reactive event
 
   datasets <- reactiveValues()
-    #read indeed data
-    observeEvent (input$go, {datasets$indeed_data <- readRDS(input$file1$datapath)
-    })
+  #read indeed data
+  observeEvent (input$go, {datasets$indeed_data <- readRDS(input$file1$datapath)
+  })
+  #create new indeed data frame based on user filter choice
+  observeEvent(input$choose,{
+    datasets$indeed_data2 <- datasets$indeed_data %>%
+      filter(category %in% mytext$text)
+  })
 
-    observeEvent(input$choose,{
-      datasets$indeed_data2 <- datasets$indeed_data %>%
-        filter(category %in% mytext$text)
-        })
-
-    mytext <- reactiveValues()
-
-    observeEvent(input$category, {
+  mytext <- reactiveValues()
+  #create a reactive character list of filters to use for map
+  observeEvent(input$category, {
     mytext$text <- c(as.character(input$category))
-    })
+  })
 
-    maps <- reactiveValues()
+  maps <- reactiveValues()
 
   observeEvent(input$map,{
-   maps$map <- if (input$map == "Map With All Job Types"){
+    #create a different map based on the choice of the user
+    maps$map <- if (input$map == "Map With All Job Types"){
       output$map <-  renderLeaflet({
 
         datasets[["indeed_data"]] %>%
           indeed_map()
       })
-      } else if (input$map == "Map With Selected Categories"){
-        output$map <-  renderLeaflet({
+    } else if (input$map == "Map With Selected Categories"){
+      output$map <-  renderLeaflet({
 
-          datasets[["indeed_data2"]] %>%
-            indeed_map()
-    })
-      }
+        datasets[["indeed_data2"]] %>%
+          indeed_map()
+      })
+    }
   })
 
-  })
+})
