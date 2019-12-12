bb03a7b1a37888f22bc4f21544d4f09c89ebf5f2
Ferdi28 <49550626+Ferdi28@users.noreply.github.com>
2019-12-12 14:49:37 +0100

    Update server.R

--- inst/shiny-examples/Indeed_search/server.R
+++ inst/shiny-examples/Indeed_search/server.R
@@ -13,21 +13,6 @@ shinyServer(function(input, output) {
   #load data as a reactive event
 
   datasets <- reactiveValues()
-    #read indeed data
-    observeEvent (input$go, {datasets$indeed_data <- readRDS(input$file1$datapath)
-    })
-    #read cities_coord data
-    observeEvent (input$go, {datasets$cities_coord <- readRDS(input$file2$datapath)
-    })
-
-
-
-  output$Map <-  renderLeaflet({
-
-    datasets[["indeed_data"]] %>%
-      map_ready_df(datasets[["cities_coord"]]) %>%
-      indeed_map()
-    })
   #read indeed data
   observeEvent (input$go, {datasets$indeed_data <- readRDS(input$file1$datapath)
   })
